315646d0b8cc2652ee22d1c5011dbf53f414d808
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:54:56 +0300

    Create 20230827180206_AddMusteriPasswordProp.cs

---
+++ Migrations/20230827180206_AddMusteriPasswordProp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMusteriPasswordProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MusteriSifre",
+                table: "Musteriler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MusteriSifre",
+                table: "Musteriler");
+        }
+    }
+}
