ecd6dcdb02b000d9f3baa0fce5cc77857d1debe0
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:56:01 +0300

    Create 20230828195346_SiparislerTableSiparisTarihiVariableStringToDateTime.cs

---
+++ Migrations/20230828195346_SiparislerTableSiparisTarihiVariableStringToDateTime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class SiparislerTableSiparisTarihiVariableStringToDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Fiyat",
+                table: "Urunler",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SiparisTarihi",
+                table: "Siparisler",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Fiyat",
+                table: "Urunler",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SiparisTarihi",
+                table: "Siparisler",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
