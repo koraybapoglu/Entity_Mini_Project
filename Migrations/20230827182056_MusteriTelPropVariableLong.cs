2400aa445f7a65592280cce5d6e0cf5f3b3f3445
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:55:04 +0300

    Create 20230827182056_MusteriTelPropVariableLong.cs

---
+++ Migrations/20230827182056_MusteriTelPropVariableLong.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class MusteriTelPropVariableLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "MusteriTel",
+                table: "Musteriler",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MusteriTel",
+                table: "Musteriler",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
