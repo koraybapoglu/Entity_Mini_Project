d2b88ae5bb901a9a83c3c8d91814dfa0691259d9
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:55:25 +0300

    Create 20230827182812_SiparisTableNameSiparisler.cs

---
+++ Migrations/20230827182812_SiparisTableNameSiparisler.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class SiparisTableNameSiparisler : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Musteriler_Siparis_SiparisID",
+                table: "Musteriler");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Siparis",
+                table: "Siparis");
+
+            migrationBuilder.RenameTable(
+                name: "Siparis",
+                newName: "Siparisler");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Siparisler",
+                table: "Siparisler",
+                column: "SiparisID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Musteriler_Siparisler_SiparisID",
+                table: "Musteriler",
+                column: "SiparisID",
+                principalTable: "Siparisler",
+                principalColumn: "SiparisID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Musteriler_Siparisler_SiparisID",
+                table: "Musteriler");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Siparisler",
+                table: "Siparisler");
+
+            migrationBuilder.RenameTable(
+                name: "Siparisler",
+                newName: "Siparis");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Siparis",
+                table: "Siparis",
+                column: "SiparisID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Musteriler_Siparis_SiparisID",
+                table: "Musteriler",
+                column: "SiparisID",
+                principalTable: "Siparis",
+                principalColumn: "SiparisID");
+        }
+    }
+}
