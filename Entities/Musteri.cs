6bd60862793f41a3fbb9bee78935c6c94b656b68
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:56:36 +0300

    Create Musteri.cs

---
+++ Entities/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Musteri_Siparis_Project.Entities
+{
+	class Musteri
+	{
+		public int MusteriID { get; set; }
+		public string MusteriAdi { get; set; }
+		public string MusteriSoyadi { get; set; }
+		public string MusteriEmail { get; set; }
+		public long MusteriTel { get; set; }
+		public string MusteriAdres { get; set; }
+		public string MusteriSifre { get; set; }
+		public Siparis? Siparis { get; set; }
+	}
+}
