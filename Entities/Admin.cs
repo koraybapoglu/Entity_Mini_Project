c8d6436faf2083f23d82f5574d4e91cc507ec02c
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:56:32 +0300

    Create Admin.cs

---
+++ Entities/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Musteri_Siparis_Project.Entities
+{
+	class Admin
+	{
+		public int AdminID { get; set; }
+		public string AdminUserName { get; set; }
+		public string AdminPassword { get; set; }
+	}
+}
