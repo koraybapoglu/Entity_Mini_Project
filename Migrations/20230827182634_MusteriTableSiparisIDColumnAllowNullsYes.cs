562e9a741142c402b33fdbd4e3cc5942f815808a
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:55:18 +0300

    Create 20230827182634_MusteriTableSiparisIDColumnAllowNullsYes.cs

---
+++ Migrations/20230827182634_MusteriTableSiparisIDColumnAllowNullsYes.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class MusteriTableSiparisIDColumnAllowNullsYes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Musteriler_Siparis_SiparisID",
+                table: "Musteriler");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SiparisID",
+                table: "Musteriler",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Musteriler_Siparis_SiparisID",
+                table: "Musteriler",
+                column: "SiparisID",
+                principalTable: "Siparis",
+                principalColumn: "SiparisID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Musteriler_Siparis_SiparisID",
+                table: "Musteriler");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SiparisID",
+                table: "Musteriler",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Musteriler_Siparis_SiparisID",
+                table: "Musteriler",
+                column: "SiparisID",
+                principalTable: "Siparis",
+                principalColumn: "SiparisID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
