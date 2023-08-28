8f07ca6e9ba4b56a7fc8443fc59b2da45e21b887
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:55:42 +0300

    Create 20230827185104_AdminPasswordVariableString.cs

---
+++ Migrations/20230827185104_AdminPasswordVariableString.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminPasswordVariableString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdminPassword",
+                table: "Admin",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AdminPassword",
+                table: "Admin",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
