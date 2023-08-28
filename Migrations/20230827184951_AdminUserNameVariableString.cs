9a3f7ad7e8c22531e9bcc05a9121de60d3a4c90f
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:55:34 +0300

    Create 20230827184951_AdminUserNameVariableString.cs

---
+++ Migrations/20230827184951_AdminUserNameVariableString.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminUserNameVariableString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdminUserName",
+                table: "Admin",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AdminUserName",
+                table: "Admin",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
