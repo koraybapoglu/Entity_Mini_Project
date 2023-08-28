31f5fbac8fc50bdb1cc485b0c42c19b7495fcb8d
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:56:44 +0300

    Create Urun.cs

---
+++ Entities/Urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Musteri_Siparis_Project.Entities
+{
+	internal class Urun
+	{
+        public int UrunID { get; set; }
+        public string UrunAdi { get; set; }
+        public double Fiyat { get; set; }
+    }
+}
