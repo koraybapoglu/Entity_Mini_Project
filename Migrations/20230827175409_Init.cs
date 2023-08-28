6c2719749fb7b18df079b885ff4146fb431ba714
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:54:49 +0300

    Create 20230827175409_Init.cs

---
+++ Migrations/20230827175409_Init.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Musteri_Siparis_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    AdminID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AdminUserName = table.Column<int>(type: "int", nullable: false),
+                    AdminPassword = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.AdminID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Siparis",
+                columns: table => new
+                {
+                    SiparisID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SiparisTarihi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SiparisDurumu = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrunID = table.Column<int>(type: "int", nullable: false),
+                    MusteriID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Siparis", x => x.SiparisID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Musteriler",
+                columns: table => new
+                {
+                    MusteriID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MusteriAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MusteriSoyadi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MusteriEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MusteriTel = table.Column<int>(type: "int", nullable: false),
+                    MusteriAdres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SiparisID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musteriler", x => x.MusteriID);
+                    table.ForeignKey(
+                        name: "FK_Musteriler_Siparis_SiparisID",
+                        column: x => x.SiparisID,
+                        principalTable: "Siparis",
+                        principalColumn: "SiparisID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Musteriler_SiparisID",
+                table: "Musteriler",
+                column: "SiparisID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "Musteriler");
+
+            migrationBuilder.DropTable(
+                name: "Siparis");
+        }
+    }
+}
