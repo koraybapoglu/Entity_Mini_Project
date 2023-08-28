ebe384758f91a507835ad78fe530c9e9a799380d
Koray Bapoğlu <koray06_61@hotmail.com>
2023-08-28 23:53:43 +0300

    Create AdminService.cs

---
+++ DataServices/AdminService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Musteri_Siparis_Project.DataServices
+{
+    internal class AdminService
+    {
+        public static bool AdminGiris()
+        {
+            using (var context = new MusteriSiparisDbContext())
+            {
+                Console.WriteLine("Kullanıcı Adını Giriniz:");
+                string adminkullaniciadi = Console.ReadLine();
+
+                Console.WriteLine("Şifreyi Giriniz:");
+                string adminSifre = Console.ReadLine();
+
+                Entities.Admin admin = context.Admin.FirstOrDefault(u => u.AdminUserName == adminkullaniciadi && u.AdminPassword == adminSifre);
+
+                if (admin != null)
+                {
+                    Console.WriteLine("Giriş Başarılı.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Giriş Başarısız. Lütfen Tekrar Deneyiniz.");
+                    return false;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
